8f7a8f1c2bd19c50693da78b304c8eeab6894325
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-07-02 21:18:26 +0530

    Created child process with namespaces

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/debarshee2004/go-container-from-scratch
 
-go 1.23.6
+go 1.23
--- main.go
+++ main.go
@@ -9,27 +9,43 @@ import (
 
 // docker run <image> <command> <args>
 
-func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
+func run() {
+	fmt.Printf("Running the container with command: %v - %d\n", os.Args[2:], os.Getpid())
 
-	default:
-		panic("Unknown command: " + os.Args[1])
+	cmd := exec.Command("/proc/self/exc", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWPID, // New container namespace
 	}
+
+	cmd.Run()
 }
 
-func run() {
-	fmt.Println("Running the container with command:", os.Args[2:])
+func child() {
+	fmt.Printf("Running the container with command: %v - %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWNS, // New container namespace
-	}
-
 	cmd.Run()
+}
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+
+	default:
+		panic("Unknown command: " + os.Args[1])
+	}
 }
